e4ec22bca351b1233c3ab68b67a380b96f763fde
danasevcik@hotmail.com <danasevcik@hotmail.com>
2018-09-10 21:16:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  if (lowercase.toLowerCase() === lowercase) {
+    return "I can't hear you!";
+  }
+}
+  sayHiToGrandma('hello')
+
+function sayHiToGrandma(string) {
+  var uppercase = 'HELLO'
+  if (uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+  sayHiToGrandma('HELLO')
+}
+
+ 
+function sayHiToGrandma(string) {
+ if (string === 'I love you, Grandma.') {
+   return("I love you, too.")
+ }
+}
+ sayHiToGrandma('I love you, Grandma.')
