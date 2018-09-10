6661e7e8605d126e87f463a0ebf1453537e48f48
danasevcik@hotmail.com <danasevcik@hotmail.com>
2018-09-10 21:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello"
-  if (lowercase.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
 }
